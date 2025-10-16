94d497931c7889af04b95c2f99956dba6baba54a
PixlGalaxy <fabrizio.gamboa.p@gmail.com>
2025-10-15 21:15:58 -0400

    chore(frontend): update ChatPage.jsx to remove react-syntax-highlighter import

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -2,8 +2,6 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { BeatLoader } from 'react-spinners';
 
 /**
